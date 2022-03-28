e798065e21ed1ee42d6a1d7917927a04017fb09b
NantapattP <nantapatt.cuvet@gmail.com>
2022-03-28 13:42:42 +0700

    ???

---
+++ Clean.py
@@ -0,0 +1,24 @@
+import pandas as pd
+df = pd.read_csv('BlueEyeExportDemo Account 1-20220323205407.csv', encoding='utf-16-le', sep='\t')
+#df.columns
+
+df = df.drop(columns=['Msg ID', 'Sentiment', 'Text', 'Author'])
+
+#seperate 2 df
+#cow df
+df = df.loc[(df['Keyword'] == 'ลัมปีสกิน') | (df['Keyword'] == 'Lumpy skin disease') | (df['Keyword'] == 'วัวป่วย') | (df['Keyword'] == 'FMD')| (df['Keyword'] == 'Foot and mouth disease') | (df['Keyword'] == 'ปากและเท้าเปื่อย')]
+df.drop_duplicates('Body', inplace=True)
+df.reset_index(drop=True, inplace=True)
+print(df)
+
+# df.to_csv('cattle.csv', index=False)
+# df.to_excel('cattle_excel.xlsx', index=False)
+
+#pig df
+new_df = df.loc[(df['Keyword'] == 'ASF') | (df['Keyword'] == 'African swine fever') | (df['Keyword'] == 'อหิวาต์แอฟริกาในสุกร') | (df['Keyword'] == 'หมูป่วย')]
+new_df.drop_duplicates('Body', inplace=True)
+new_df.reset_index(inplace =True, drop=True)
+print(new_df)
+
+# new_df.to_csv('pig.csv', index=False)
+# new_df.to_excel('pig_excel.xlsx', index=False)
